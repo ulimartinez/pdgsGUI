2b7bef5dec48bc1b4885e088614bf7bea268fcb8
vrivas28 <31858274+vrivas28@users.noreply.github.com>
2018-03-22 14:03:06 -0600

    Added small changes

--- PGDS/PCAPOverlayDia.py
+++ PGDS/PCAPOverlayDia.py
@@ -113,8 +113,8 @@ class PCAPOverlayDia(Gtk.Dialog):
 ##
 ##        dialog.destroy()
 
-win = PCAPOverlayWin()
-win.connect("delete-event", Gtk.main_quit)
-win.show_all()
-Gtk.main()
+##win = PCAPOverlayWin()
+##win.connect("delete-event", Gtk.main_quit)
+##win.show_all()
+##Gtk.main()
 
